3c2e4a6f355368c2adcf5a8fdfdff3827117bb71
chandan <chandansingh.tis@gmail.com>
2026-03-25 15:43:27 +0530

    updated logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -103,7 +103,7 @@ export default function Header({
         <div className="flex items-center gap-3 ml-auto">
 
           {/* SEARCH */}
-          <div className="relative hidden md:block">
+          <div className="relative hidden">
             <Search className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
             <Input
               placeholder="Search…"
